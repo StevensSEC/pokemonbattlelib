428d43f401eb2729f9ada2155e1c8c3c4923c131
Carson McManus <carson.mcmanus1@gmail.com>
2021-02-06 18:31:17 -0500

    add a method to get pokemon by national dex number

--- pokedex.go
+++ pokedex.go
@@ -1,3 +1,14 @@
 package pokemonbattlelib
 
 //go:generate go run data/gen.go
+
+// Get a new pokemon by nation dex number.
+func GetPokemon(natdex uint16) Pokemon {
+	for _, p := range ALL_POKEMON {
+		if p.NatDex == natdex {
+			return p
+		}
+	}
+	// Not exactly the best way to handle this
+	return Pokemon{}
+}
