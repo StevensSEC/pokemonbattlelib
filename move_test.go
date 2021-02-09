7925ddaa6ecf71419143dde12aced5d14a0efcdc
A-very-Cunning-ham <ave210knife@gmail.com>
2021-02-09 12:50:15 -0500

    Update move_test.go with suggested change

--- move_test.go
+++ move_test.go
@@ -1,12 +1,15 @@
 package pokemonbattlelib
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMoveString(t *testing.T) {
 	m := Move{
 		Name:     "Shadow Ball",
 		Type:     8,
-		Category: 3,
+		Category: Special,
 		Max_PP:   15,
 		Priority: 0,
 		Power:    80,
@@ -18,15 +21,30 @@ func TestMoveString(t *testing.T) {
 }
 
 func TestMoveCategoryString(t *testing.T) {
-	var c1, c2, c3 MoveCategory = 1, 2, 3
-
-	if c1.String() != "Status" {
-		t.Fail()
+	tests := []struct {
+		value MoveCategory
+		want  string
+	}{
+		{
+			value: Status,
+			want:  "Status",
+		},
+		{
+			value: Physical,
+			want:  "Physical",
+		},
+		{
+			value: Special,
+			want:  "Special",
+		},
 	}
-	if c2.String() != "Physical" {
-		t.Fail()
-	}
-	if c3.String() != "Special" {
-		t.Fail()
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("MoveCategory Stringer: %s", tt.want), func(t *testing.T) {
+			t.Parallel()
+			got := tt.value.String()
+			if got != tt.want {
+				t.Errorf("Move Category (%d) got %v, want %v", tt.value, got, tt.want)
+			}
+		})
 	}
 }
