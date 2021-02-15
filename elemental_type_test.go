1a4f975fa6fe3fa2b2f5514cbc25e1355c958b2d
Carson McManus <carson.mcmanus1@gmail.com>
2021-02-15 13:55:46 -0500

    update Pokemon and Move to use ElementalType

--- elemental_type_test.go
+++ elemental_type_test.go
@@ -49,7 +49,7 @@ func TestGetEffect(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("Type Effectiveness: %v vs %v", tt.move, tt.def), func(t *testing.T) {
-			// t.Parallel()
+			t.Parallel()
 			got := GetEffect(tt.move, tt.def)
 			if got != tt.want {
 				t.Errorf("Type Effectiveness: %v vs %v should be %f, got %f", tt.move, tt.def, tt.want, got)
