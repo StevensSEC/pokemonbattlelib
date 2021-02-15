2ef26d0b67d45e98e96383df19abfe3c88e0a946
Carson McManus <carson.mcmanus1@gmail.com>
2021-02-15 14:19:44 -0500

    remove unnecessary t.Parallel() because it was affecting test coverage

--- elemental_type_test.go
+++ elemental_type_test.go
@@ -49,7 +49,6 @@ func TestGetEffect(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("Type Effectiveness: %v vs %v", tt.move, tt.def), func(t *testing.T) {
-			t.Parallel()
 			got := GetEffect(tt.move, tt.def)
 			if got != tt.want {
 				t.Errorf("Type Effectiveness: %v vs %v should be %f, got %f", tt.move, tt.def, tt.want, got)
@@ -82,7 +81,6 @@ func TestElementalTypeString(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("ElementalType Stringer: %s", tt.want), func(t *testing.T) {
-			t.Parallel()
 			got := fmt.Sprintf("%s", tt.value)
 			if got != tt.want {
 				t.Errorf("ElementalType (%d) got %v, want %v", tt.value, got, tt.want)
