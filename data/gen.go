4ac664e9814aff0090cde9a113f757aeda0af5b9
Carson McManus <carson.mcmanus1@gmail.com>
2021-02-21 00:11:21 -0500

    add ItemFlags, and update codegen

--- data/gen.go
+++ data/gen.go
@@ -57,6 +57,7 @@ type data_item struct {
 	CategoryID    int
 	FlingPower    int
 	FlingEffectID int
+	Flags         []string
 }
 
 func parseInt(s string) (n int) {
@@ -371,10 +372,17 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
-	valid_items := make(map[string]bool)
+	itemFlagMap := map[string]string{
+		"2": "FlagConsumable",
+		"4": "FlagUsableInBattle",
+		"5": "FlagHoldable",
+		"6": "FlagHoldablePassive",
+		"7": "FlagHoldableActive",
+	}
+	item_flags := make(map[string][]string)
 	for _, r := range records {
 		if r[1] == "4" || r[1] == "5" || r[1] == "7" {
-			valid_items[r[0]] = true
+			item_flags[r[0]] = append(item_flags[r[0]], itemFlagMap[r[1]])
 		}
 	}
 	output += "// Create item constant enum for quick reference\nconst (\n"
@@ -386,7 +394,7 @@ func main() {
 	}
 	// Add item names
 	for _, r := range records {
-		if _, ok := valid_items[r[0]]; !ok {
+		if _, ok := item_flags[r[0]]; !ok {
 			continue
 		}
 		if parseInt(r[1]) != ENGLISH_LANGUAGE_ID {
@@ -411,7 +419,7 @@ func main() {
 		log.Panicln(err)
 	}
 	for _, r := range records {
-		if _, ok := valid_items[r[0]]; !ok {
+		if _, ok := item_flags[r[0]]; !ok {
 			continue
 		}
 		item := data_item{
@@ -420,14 +428,15 @@ func main() {
 			CategoryID:    parseInt(r[2]),
 			FlingPower:    parseInt(r[4]),
 			FlingEffectID: parseInt(r[5]),
+			Flags:         item_flags[r[0]],
 		}
 		items = append(items, item)
 	}
 	// Add item data to generated output
 	output += "// A collection of all items in the game\n" + "var ALL_ITEMS = []Item{\n"
 	for _, item := range items {
-		output += fmt.Sprintf("\t{ID: %s, Name: \"%s\", Category: %d, FlingPower: %d, FlingEffect: %d},\n",
-			item.ID, item_names[item.ID], item.CategoryID, item.FlingPower, item.FlingEffectID)
+		output += fmt.Sprintf("\t{ID: %s, Name: \"%s\", Category: %d, FlingPower: %d, FlingEffect: %d, Flags: %s},\n",
+			item.ID, item_names[item.ID], item.CategoryID, item.FlingPower, item.FlingEffectID, strings.Join(item.Flags, " | "))
 	}
 	output += "}\n\n"
 
