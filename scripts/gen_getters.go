b1a03b6cfbb37985be62444a93a7fc4e2c3b33d8
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-12 19:26:25 -0500

    improve getter generation

--- scripts/gen_getters.go
+++ scripts/gen_getters.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	forTypeName    = flag.String("for", "", "The type to create getter functions for.")
-	dataVarName    = flag.String("data", "", "The variable that contains all the constant data for the type")
+	dataTypeName   = flag.String("data", "", "The variable that contains all the constant data for the type")
 	outputFileName = flag.String("output", "", "output file name; default <for>_getters_GEN.go")
 )
 
@@ -38,7 +38,7 @@ func main() {
 	if *outputFileName == "" {
 		*outputFileName = fmt.Sprintf("%s_getters_GEN.go", strings.ToLower(*forTypeName))
 	}
-	log.Printf("Generating getters on type %s for data %s", *forTypeName, *dataVarName)
+	log.Printf("Generating getters on type %s for data %s", *forTypeName, *dataTypeName)
 
 	fset := token.NewFileSet()
 
@@ -57,57 +57,33 @@ func main() {
 
 	// find the forType and the data
 	var forType *ast.TypeSpec
-	var dataVar *ast.ValueSpec
-	var dataVarTypeName string
-	var dataVarType *ast.TypeSpec
-	// Yeah, I know this is pretty inefficient, but this AST library is hard to use and this was the easiest solution.
-	// It would be better if you could resolve the identifier for dataVar to turn it into a TypeSpec, but there's
-	// basically no good examples for how to use this package in the official docs.
-	for forType == nil || dataVar == nil || dataVarType == nil {
-		for _, d := range libFile.Decls {
-			switch n := d.(type) {
-			case *ast.GenDecl:
-				for _, s := range n.Specs {
-					switch n := s.(type) {
-					case *ast.TypeSpec:
-						if forType == nil && n.Name.Name == *forTypeName {
-							log.Printf("found for type declaration: %v", n)
-							forType = n
-						}
-						if dataVar != nil && dataVarType == nil {
-							if n.Name.Name == dataVarTypeName {
-								log.Printf("found data var type declaration: %v", n)
-								dataVarType = n
-							}
-						}
-					case *ast.ValueSpec:
-						if dataVar == nil && n.Names[0].Name == *dataVarName {
-							// log.Printf("found data var declaration: %v type: %#v %#v", n, n.Type, n.Values)
-							if t, ok := n.Values[0].(*ast.CompositeLit); ok {
-								dataVarTypeName = t.Type.(*ast.ArrayType).Elt.(*ast.Ident).Name
-								log.Printf("found data var declaration: %v type: %v", t.Type, dataVarTypeName)
-								dataVar = n
-							}
-						}
-					default:
-						// log.Printf("spec type: %T", n)
+	var dataType *ast.TypeSpec
+	for _, d := range libFile.Decls {
+		switch n := d.(type) {
+		case *ast.GenDecl:
+			for _, s := range n.Specs {
+				switch n := s.(type) {
+				case *ast.TypeSpec:
+					if forType == nil && n.Name.Name == *forTypeName {
+						log.Printf("found for type declaration: %v", n)
+						forType = n
+					} else if dataType == nil && n.Name.Name == *dataTypeName {
+						log.Printf("found data type declaration: %v", n)
+						dataType = n
 					}
 				}
-			default:
-				// log.Printf("type: %T", n)
 			}
+		}
+		if forType != nil && dataType != nil {
+			break
 		}
 	}
 
 	// extract the fields
-	log.Printf("data var type: %#v", dataVarType.Type)
-	// fields := []struct {
-	// 	Name string
-	// 	Type string
-	// }{}
+	log.Printf("data var type: %#v", dataType.Type)
 
 	output := ""
-	for _, field := range dataVarType.Type.(*ast.StructType).Fields.List {
+	for _, field := range dataType.Type.(*ast.StructType).Fields.List {
 		log.Printf("field: %v %v", field.Names[0], field.Type)
 		output += fmt.Sprintf("func (n %[1]s) %[2]s() %[3]s { return n.Data().%[2]s }\n", *forTypeName, field.Names[0], field.Type)
 	}
