d91020780c792ca0cd51f4c2df0a910b257da077
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-29 02:35:34 -0400

    add agent test

---
+++ agent_test.go
@@ -0,0 +1,45 @@
+package pokemonbattlelib
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type cheater struct{}
+
+func (a cheater) Act(ctx *BattleContext) Turn {
+	ctx.Pokemon.Type |= TypeDragon
+	return FightTurn{Move: 0, Target: ctx.Opponents[0]}
+}
+
+type funkyAgent func(*BattleContext) Turn
+
+func (a funkyAgent) Act(ctx *BattleContext) Turn {
+	return a(ctx)
+}
+
+var _ = Describe("Agents", func() {
+	It("should never receive a ground truth reference to pokemon in the battle", func() {
+		b := NewBattle()
+		a1 := newRcAgent()
+		_a1 := Agent(a1)
+		a2 := Agent(funkyAgent(func(ctx *BattleContext) Turn {
+			real := b.getPokemon(ctx.Opponents[0])
+			Expect(ctx.Opponents[0].Pokemon).ToNot(BeIdenticalTo(real))
+			// Expect(ctx.Opponents[0].Pokemon.Moves[0]).ToNot(BeIdenticalTo(real.Moves[0])) // See #294
+			return FightTurn{Move: 0, Target: ctx.Opponents[0]}
+		}))
+		party1 := NewOccupiedParty(&_a1, 0, GeneratePokemon(PkmnCharmander, defaultMoveOpt))
+		party2 := NewOccupiedParty(&a2, 1, GeneratePokemon(PkmnSquirtle, defaultMoveOpt))
+		b.AddParty(party1, party2)
+		Expect(b.Start()).To(Succeed())
+		a1 <- FightTurn{
+			Move: 0,
+			Target: target{
+				party:     1,
+				partySlot: 0,
+			},
+		}
+		b.SimulateRound()
+	})
+})
