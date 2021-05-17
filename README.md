322b429be091a1d0312ed971762a0b45e32d85ee
rdhadda123 <71467021+rdhadda123@users.noreply.github.com>
2021-05-17 17:03:39 -0400

    add example battle code and update readme
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    Co-authored-by: Carson McManus <carson.mcmanus1@gmail.com>

--- README.md
+++ README.md
@@ -9,6 +9,37 @@ A portable library for accurately simulating pokemon battles.
 
 [Documentation](https://pkg.go.dev/github.com/StevensSEC/pokemonbattlelib)
 
+### Basic Example Battle
+
+```go
+p1 := GeneratePokemon(PkmnPikachu,
+	WithLevel(20),
+	WithMoves(MoveThunderShock))
+p2 := GeneratePokemon(PkmnBulbasaur,
+	WithLevel(20),
+	WithMoves(MoveTackle))
+a1 := Agent(new(dumbAgent))
+b := New1v1Battle(p1, &a1, p2, &a1)
+b.Start()
+transactions, _ := b.SimulateRound()
+for _, t := range transactions {
+	switch tt := t.(type) {
+	case UseMoveTransaction:
+		fmt.Printf("%s used %s on %s\n", b.getPokemon(tt.User), tt.Move, b.getPokemon(tt.Target))
+	case DamageTransaction:
+		fmt.Printf("%s took %d damage\n", b.getPokemon(tt.Target), tt.Damage)
+	case HealTransaction:
+		fmt.Printf("%s healed for %d HP\n", b.getPokemon(tt.Target), tt.Amount)
+	case FaintTransaction:
+		fmt.Printf("%s fainted\n", b.getPokemon(tt.Target))
+	case PPTransaction:
+		continue
+	default:
+		fmt.Printf("Transaction: %T - %v\n", t, t)
+	}
+}
+```
+
 # Contributing
 
 1. Make sure that you at least [have Go 1.15](https://golang.org/dl/)
--- examples_test.go
+++ examples_test.go
@@ -2,8 +2,43 @@
 
 package pokemonbattlelib
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func ExampleBattle() {
+	p1 := GeneratePokemon(PkmnPikachu,
+		WithLevel(20),
+		WithMoves(MoveThunderShock))
+	p2 := GeneratePokemon(PkmnBulbasaur,
+		WithLevel(20),
+		WithMoves(MoveTackle))
+	a1 := Agent(new(dumbAgent))
+	b := New1v1Battle(p1, &a1, p2, &a1)
+	b.Start()
+	transactions, _ := b.SimulateRound()
+	for _, t := range transactions {
+		switch tt := t.(type) {
+		case UseMoveTransaction:
+			fmt.Printf("%s used %s on %s\n", b.getPokemon(tt.User), tt.Move, b.getPokemon(tt.Target))
+		case DamageTransaction:
+			fmt.Printf("%s took %d damage\n", b.getPokemon(tt.Target), tt.Damage)
+		case HealTransaction:
+			fmt.Printf("%s healed for %d HP\n", b.getPokemon(tt.Target), tt.Amount)
+		case FaintTransaction:
+			fmt.Printf("%s fainted\n", b.getPokemon(tt.Target))
+		case PPTransaction:
+			continue
+		default:
+			fmt.Printf("Transaction: %T - %v\n", t, t)
+		}
+	}
+	//Output:
+	//Pikachu used Thunder Shock on Bulbasaur
+	//Bulbasaur took 6 damage
+	//Bulbasaur used Tackle on Pikachu
+	//Pikachu took 11 damage
+}
 func ExampleGeneratePokemon() {
 	pkmn := GeneratePokemon(PkmnPikachu,
 		WithLevel(28),
